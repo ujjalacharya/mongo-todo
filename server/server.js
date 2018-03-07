6f6b009a84d209ebd0e33adde104abaf6d44cc00
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-03-07 13:45:03 +0545

    added /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -28,6 +29,24 @@ app.post("/todos",(req, res) =>{
     }, (err)=>{
         res.status(400).send(err);
     })
+})
+
+app.get("/todos/:id", (req, res)=>{
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }  
+
+
+Todo.findById(id).then((todo)=>{
+    if(!todo){
+        return res.status(400).send();
+    }
+    res.send({todo})
+}).catch((e)=>{
+    res.status(400)
+})
 })
 
 app.listen(PORT, ()=>{
